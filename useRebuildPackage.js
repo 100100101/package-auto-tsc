581814782128f0fac58f22f017df821ce19f3023
JSerGit <bibik.serej@gmail.com>
2023-03-09 07:10:42 +0200

    step

--- useRebuildPackage.js
+++ useRebuildPackage.js
@@ -12,9 +12,23 @@ module.exports = ({
         const pathToTsc = require.resolve('typescript/bin/tsc', {
             paths: [packageRootDir],
         })
-        execFileSync(process.execPath, [pathToTsc, '--build', pathToTsConfig], {
-            cwd: packageRootDir,
-        })
+        try {
+            execFileSync(
+                process.execPath,
+                [pathToTsc, '--build', pathToTsConfig],
+                {
+                    cwd: packageRootDir,
+                }
+            )
+        } catch (error) {
+            const { status, message, stderr, stdout } = error
+            console.error('Error package-auto-tsc:')
+            console.error('status:', status, '\n')
+            console.error('message:', message, '\n')
+            console.error('stderr:', stderr.toString(), '\n')
+            console.error('stdout:', stdout.toString(), '\n')
+            throw new Error('')
+        }
         console.log(`\x1b[46m\nuse-package-builder:\x1b[0m`)
         console.log(
             `\x1b[45mPackage has been rebuilt: \n\n${packageRootDir}\x1b[0m\n`
