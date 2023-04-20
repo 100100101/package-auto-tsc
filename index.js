85eeb7eda6794c8ed35d95712b34896c51346dfa
Sergiy <bibik.serej@gmail.com>
2023-04-20 18:17:30 +0200

    step

--- index.js
+++ index.js
@@ -3,26 +3,48 @@ const fs = require('fs')
 const checkIsNeedRebuildLevel2 = require('./checkIsNeedRebuildLevel2')
 const useRebuildPackage = require('./useRebuildPackage')
 const { DEFAULT_REBUILD_DIFF_TIME_MS } = require('./constants')
+const tsconfigFileName = 'tsconfig.json'
 const defaultOptions = {
     rebuildDiffTimeMs: DEFAULT_REBUILD_DIFF_TIME_MS,
-    isAutoIncrementVersion: true,
+    isAutoIncrementVersion: false,
 }
 module.exports = (packageRootDir, options = {}) => {
     const mergedOptions = Object.assign({}, defaultOptions, options)
     const { rebuildDiffTimeMs, isAutoIncrementVersion } = mergedOptions
 
-    const pathToTsConfig = path.join(packageRootDir, 'tsconfig.json')
+    let pathToTsConfig = path.join(packageRootDir, tsconfigFileName)
     const pathToTypescript = require.resolve('typescript', {
         paths: [packageRootDir],
     })
     if (!pathToTypescript) throw new Error('typescript module is not found')
     const ts = require(pathToTypescript)
-    const tsconfig = ts.readConfigFile(pathToTsConfig, ts.sys.readFile)
+    const tsSysReadFile = ts.sys.readFile
+    const readConfigFileResult = ts.readConfigFile(
+        pathToTsConfig,
+        tsSysReadFile
+    )
+    let tsconfig = readConfigFileResult.config
+    const readConfigFileResultError = readConfigFileResult.error
+    if (readConfigFileResultError) {
+        tsconfig = null
+        pathToTsConfig = path.join(process.cwd(), tsconfigFileName)
+        const readConfigFileResult = ts.readConfigFile(
+            pathToTsConfig,
+            tsSysReadFile
+        )
+        if (!readConfigFileResult.error) {
+            tsconfig = readConfigFileResult.config
+        }
+    }
     if (!tsconfig) throw new Error('tsconfig is not found')
-    const tsconfigOutDir = tsconfig.config.compilerOptions.outDir
-    if (!tsconfigOutDir) throw new Error('outDir is not defined')
+    let tsconfigOutDir = tsconfig.compilerOptions.outDir
+    if (!tsconfigOutDir) {
+        // throw new Error('outDir is not defined')
+        tsconfigOutDir = './dist'
+    }
     const tsconfigOutDirAbs = path.join(packageRootDir, tsconfigOutDir)
     const isOutDirExists = fs.existsSync(tsconfigOutDirAbs)
+
     const rebuildPackage = useRebuildPackage({
         tsconfigOutDirAbs,
         packageRootDir,
